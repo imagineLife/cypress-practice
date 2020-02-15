268619d489a7d9a7abb9bfa9434f41764f9e491d
Eric Laursen <ericjlaursen@gmail.com>
2020-02-14 20:28:57 -0500

    tests first todo item

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -2,4 +2,12 @@ describe('APP', () => {
 	it('loads the app @ root url', () => {
 		cy.visit('/')
 	})
+
+	it('gets first to-do item', () => {
+		cy.visit('/')
+		
+		const firstItem = cy.get('.todo-list li:nth-child(1)')
+
+		firstItem.should('have.text', 'Goodnight moon')
+	})
 })
