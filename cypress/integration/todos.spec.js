d0417eb18f1af55bb2684997a0f3e9e79f983089
Eric Laursen <ericjlaursen@gmail.com>
2020-02-15 13:04:12 -0500

    linted

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,46 +1,46 @@
 describe('APP loads', () => {
-	it('loads the app @ root url', () => {
-		cy.visit('/')
-	})
+  it('loads the app @ root url', () => {
+    cy.visit('/');
+  });
 
-	it('gets first to-do item', () => {
-		// cy.visit('/')
-		
-		//WITHOUT using data-cy attr
-		// const firstItem = cy.get('.todo-list li:nth-child(1)')
+  it('gets first to-do item', () => {
+    // cy.visit('/')
 
-		//USING data-cy attr
-		const firstItem = cy.get('[data-cy=todo-item-8]')
+    // WITHOUT using data-cy attr
+    // const firstItem = cy.get('.todo-list li:nth-child(1)')
 
-		let myVar = 'stringVal'
-		let numVar = 9;
-		firstItem
-			//DEBUGGING
-			// .then(e => {debugger;})
+    // USING data-cy attr
+    const firstItem = cy.get('[data-cy=todo-item-8]');
 
-			.should('have.text', 'juice the box')
-		firstItem.should('not.have.class', 'completed')
+    const myVar = 'stringVal';
+    const numVar = 9;
+    firstItem
+    // DEBUGGING
+    // .then(e => {debugger;})
 
-		//Debugging again!
-		// firstItem.debug()
-	})
+      .should('have.text', 'juice the box');
+    firstItem.should('not.have.class', 'completed');
 
-	it('first check-box is NOT checked', () => {
-		// cy.visit('/')
-		
-		// const firstCheckBox = cy.get('.todo-list li:nth-child(1) .toggle')
-		const firstCheckBox = cy.get('[data-cy=todo-item-8] .toggle')
-		firstCheckBox.should('not.be.checked')
-	})
+    // Debugging again!
+    // firstItem.debug()
+  });
 
-	it('second check-box IS checked', () => {
-		// cy.visit('/')
-		
-		// const secondCheckBox = cy.get('.todo-list li:nth-child(2) .toggle')
-		const secondCheckBox = cy.get('[data-cy=todo-item-9] .toggle')
-		secondCheckBox.should('be.checked')
-	})
-})
+  it('first check-box is NOT checked', () => {
+    // cy.visit('/')
+
+    // const firstCheckBox = cy.get('.todo-list li:nth-child(1) .toggle')
+    const firstCheckBox = cy.get('[data-cy=todo-item-8] .toggle');
+    firstCheckBox.should('not.be.checked');
+  });
+
+  it('second check-box IS checked', () => {
+    // cy.visit('/')
+
+    // const secondCheckBox = cy.get('.todo-list li:nth-child(2) .toggle')
+    const secondCheckBox = cy.get('[data-cy=todo-item-9] .toggle');
+    secondCheckBox.should('be.checked');
+  });
+});
 
 /*
 	CYPRESS PROCESS OVERVIEW
