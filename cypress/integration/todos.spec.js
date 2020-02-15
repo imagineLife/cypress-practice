186cb7b1d2da3791031107639a21358605a5e6ee
Eric Laursen <ericjlaursen@gmail.com>
2020-02-15 08:53:27 -0500

    removes re-visiting

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -4,7 +4,7 @@ describe('APP loads', () => {
 	})
 
 	it('gets first to-do item', () => {
-		cy.visit('/')
+		// cy.visit('/')
 		
 		//WITHOUT using data-cy attr
 		// const firstItem = cy.get('.todo-list li:nth-child(1)')
@@ -17,7 +17,7 @@ describe('APP loads', () => {
 	})
 
 	it('first check-box is NOT checked', () => {
-		cy.visit('/')
+		// cy.visit('/')
 		
 		// const firstCheckBox = cy.get('.todo-list li:nth-child(1) .toggle')
 		const firstCheckBox = cy.get('[data-cy=todo-item-8] .toggle')
@@ -25,7 +25,7 @@ describe('APP loads', () => {
 	})
 
 	it('second check-box IS checked', () => {
-		cy.visit('/')
+		// cy.visit('/')
 		
 		// const secondCheckBox = cy.get('.todo-list li:nth-child(2) .toggle')
 		const secondCheckBox = cy.get('[data-cy=todo-item-9] .toggle')
