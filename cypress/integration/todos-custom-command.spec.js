73c34fb885b9dc6f5f4b13f4fcfeacddad39b8e0
Eric Laursen <ericjlaursen@gmail.com>
2020-02-20 20:38:10 -0500

    more content

---
+++ cypress/integration/todos-custom-command.spec.js
@@ -0,0 +1,43 @@
+describe('Validates redux store vals', () => {
+  it('has matching content', () => {
+    /*
+      See cypress/support/commands.js for custom command
+
+    */
+    cy.visit('/');
+
+    const win = cy.window();
+
+    // access properties on window using its
+    // https://docs.cypress.io/api/commands/its.html#Syntax
+    const store = win.its('store');
+
+    // get state of the store
+    const storeState = store.invoke('getState');
+
+    // show the store in the cypress test window
+    cy.cypressStore;
+
+    // this assures that the to-dos have loaded into the store
+    storeState.should('deep.equal', {
+      todos: [
+        {
+          text: 'juice the box',
+          completed: false,
+          id: 8,
+        },
+        {
+          id: 9,
+          completed: true,
+          text: 'talk to deenzy',
+        },
+        {
+          text: 'sink the ship',
+          completed: false,
+          id: 10,
+        },
+      ],
+      visibilityFilter: 'show_all',
+    });
+  });
+});
